33bfb98e45c33510698c06719d226438417afad3
Iana Sokolova <iana_sokolova@epam.com>
2021-04-11 21:50:29 +0300

    Added support request by city

--- src/main/java/com/example/weatherbot/app/model/User.java
+++ src/main/java/com/example/weatherbot/app/model/User.java
@@ -1,19 +1,59 @@
 package com.example.weatherbot.app.model;
 
-import org.telegram.telegrambots.meta.api.objects.Location;
-
 //@Document
 public class User {
-   // @Field
+    // @Field
     private String userName;
-   // @Field
+    // @Field
     private Location location;
-   // @Id
+    // @Id
     private long chatId;
+    //@Field
+    private String city;
 
+    public static class Location {
+
+        public Location(float lat, float lon) {
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        private float lat;
+        private float lon;
+
+        public float getLat() {
+            return lat;
+        }
+
+        public void setLat(float lat) {
+            this.lat = lat;
+        }
+
+        public float getLon() {
+            return lon;
+        }
+
+        public void setLon(float lon) {
+            this.lon = lon;
+        }
+
+        @Override
+        public String toString() {
+            return "Location{" +
+                    "lat=" + lat +
+                    ", lon=" + lon +
+                    '}';
+        }
+    }
     public User(String userName, Location location, long chatId) {
         this.userName = userName;
         this.location = location;
+        this.chatId = chatId;
+    }
+
+    public User(String userName, String city, long chatId) {
+        this.userName = userName;
+        this.city = city;
         this.chatId = chatId;
     }
 
@@ -40,4 +80,22 @@ public class User {
     public void setChatIid(long chatId) {
         this.chatId = chatId;
     }
+
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", location=" + location +
+                ", chatId=" + chatId +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
+
--- src/main/java/com/example/weatherbot/app/service/WeatherService.java
+++ src/main/java/com/example/weatherbot/app/service/WeatherService.java
@@ -1,17 +1,20 @@
 package com.example.weatherbot.app.service;
 
+import com.example.weatherbot.app.dto.ForecastDto;
 import com.example.weatherbot.app.dto.WeatherDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 
-@Component
-@ConfigurationProperties(prefix = "openweather")
+
+@PropertySource("classpath:application.properties")
+@Configuration
 public class WeatherService {
     private final RestTemplate restTemplate;
-    private String token;
+
+    private String token = "267f70c609cff8699fdc74f50434b9c4";
 
     @Autowired
     public WeatherService(RestTemplate restTemplate) {
@@ -31,7 +34,7 @@ public class WeatherService {
         current weather by city name from service "Open Weather"
          */
     public WeatherDto getCurrentWeatherFromOWByCity(String cityName) {
-        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "+&appid=" + token;
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&appid=" + token;
         return restTemplate.getForObject(url, WeatherDto.class);
 
     }
@@ -48,10 +51,10 @@ public class WeatherService {
     /*
     forecast weather for the next day from by location service "Open Weather"
      */
-    public WeatherDto getForecastWeatherFromOWByLocation(Float lat, Float lon, int days) {
+    public ForecastDto getForecastWeatherFromOWByLocation(Float lat, Float lon, int days) {
         String url = "http://api.openweathermap.org/data/2.5/forecast/?lat=" + lat + "&lon=" + lon
                 + days + "&cnt=8&units=metric&appid=" + token;
-        return restTemplate.getForObject(url, WeatherDto.class);
+        return restTemplate.getForObject(url, ForecastDto.class);
 
     }
 
@@ -67,10 +70,10 @@ public class WeatherService {
     /*
   forecast weather by location name from service "Яндекс погода"
   */
-    public WeatherDto getForecastWeatherFromYNByLocation(Float lat, Float lon, int days) {
+    public ForecastDto getForecastWeatherFromYNByLocation(Float lat, Float lon, int days) {
         String url = "https://api.weather.yandex.ru/v2/forecast?lat=" + lat + "&lon=" + lon + "&extra=true " +
                 days + "X-Yandex-API-Key: fffff866-6141-46fb-aa27-9cc943366a62\n";
-        return restTemplate.getForObject(url, WeatherDto.class);
+        return restTemplate.getForObject(url, ForecastDto.class);
     }
 
 }
