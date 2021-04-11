620cf8ca3c2fa814c36ea7273ecabedfa06ebfcf
Egor Matveev <egor_matveev@epam.com>
2021-04-11 20:22:49 +0300

    Refactoring methods toString, add parameter condition, add method "cityIsNotEmpty", added 3-d Weather API

--- src/main/java/com/example/weatherbot/app/model/Forecast.java
+++ src/main/java/com/example/weatherbot/app/model/Forecast.java
@@ -1,25 +1,24 @@
 package com.example.weatherbot.app.model;
 
 import com.example.weatherbot.app.dto.ForecastDto;
-
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Forecast {
+    private final List condition;
     private final String cityName;
     private final int days;
     private final List<DayForecast> forecasts = new ArrayList<>();
 
     public Forecast(ForecastDto dto) {
-
+        this.condition = dto.getCondition();
         this.cityName = dto.getCityName();
         this.days = dto.getDays();
 
         for (int i = 0; i < days; i++) {
-            forecasts.add(new DayForecast(
+            forecasts.add(new DayForecast(dto.getCondition().get(i),
                     String.format("%.0f°C", dto.getDayTemp().get(i)),
                     String.format("%.0f°C", dto.getNightTemp().get(i)),
                     LocalDate.now().plusDays(i)
@@ -40,12 +39,14 @@ public class Forecast {
     }
 
     public static class DayForecast {
+        private final String condition;
         private final String dayTemp;
         private final String nightTemp;
         private final LocalDate localDate;
 
-        public DayForecast(String dayTemp,
+        public DayForecast(String condition, String dayTemp,
                            String nightTemp, LocalDate localDate) {
+            this.condition = condition;
             this.dayTemp = dayTemp;
             this.nightTemp = nightTemp;
             this.localDate = localDate;
@@ -53,18 +54,18 @@ public class Forecast {
 
         @Override
         public String toString() {
-            return  "dayTemp = " + this.dayTemp + '\'' +
-                    ", nightTemp = " + this.nightTemp + '\'' +
-                    ", localDate = " + this.localDate +
-                    '}';
+            return "condition = " + this.condition + '\'' +
+                    ", day temp = " + this.dayTemp + '\'' +
+                    ", night temp = " + this.nightTemp + '\'' +
+                    ", local date = " + this.localDate;
         }
     }
 
     @Override
     public String toString() {
-        return "cityName = " + this.cityName + '\'' +
+        return "condition = " + this.condition +
+                ", City name = " + this.cityName + '\'' +
                 ", days = " + this.days +
-                ", forecasts = " + this.forecasts +
-                '}';
+                ", forecasts = " + this.forecasts;
     }
 }
--- src/main/java/com/example/weatherbot/app/model/Weather.java
+++ src/main/java/com/example/weatherbot/app/model/Weather.java
@@ -3,9 +3,9 @@ package com.example.weatherbot.app.model;
 import com.example.weatherbot.app.dto.WeatherDto;
 
 
-
 public final class Weather {
     private final String cityName;
+    private final String condition;
     private final String temp;
     private final String pressure;
     private final String humidity;
@@ -16,6 +16,7 @@ public final class Weather {
         this.temp = String.format("%.0f°C", dto.getTemp());
         this.pressure = String.format("%.0f мм рт.ст.", dto.getPressure() * 3 / 4.0);
         this.humidity = dto.getHumidity() + "%";
+        this.condition = dto.getCondition();
     }
 
     public String getCityName() {
@@ -34,12 +35,16 @@ public final class Weather {
         return humidity;
     }
 
+    public String getCondition() {
+        return condition;
+    }
+
     @Override
     public String toString() {
-        return  "cityName = " + this.cityName + '\'' +
+        return  "City name = " + this.cityName + '\'' +
+                ", condition = " + this.condition + '\'' +
                 ", temp = " + this.temp + '\'' +
                 ", pressure = " + this.pressure + '\'' +
-                ", humidity = " + this.humidity + '\'' +
-                '}';
+                ", humidity = " + this.humidity + '\'';
     }
 }
